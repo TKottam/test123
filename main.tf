5fac43ffce42e374363ef1c89ca5d745abf0d689
Ashish Kumar <ashish.dav99@gmail.com>
2021-08-06 12:56:07 +0000

    updates

--- main.tf
+++ main.tf
@@ -23,7 +23,45 @@ provider "aws" {
   profile = "default"
 }
 
+variable "availability_zone_names" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"] // Put it manually instead of auto picking active ones due to availability issues of the instance types
+}
 
+resource "aws_vpc" "demo" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "demo" {
+  count                   = length(var.availability_zone_names)
+  availability_zone       = var.availability_zone_names[count.index]
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.demo.id
+}
+
+resource "aws_internet_gateway" "demo" {
+  vpc_id = aws_vpc.demo.id
+  tags = {
+    Name = "terraform-eks"
+  }
+}
+
+resource "aws_route_table" "demo" {
+  vpc_id = aws_vpc.demo.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo.id
+  }
+}
+
+resource "aws_route_table_association" "demo" {
+  count          = length(var.availability_zone_names)
+  subnet_id      = aws_subnet.demo.*.id[count.index]
+  route_table_id = aws_route_table.demo.id
+}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -31,8 +69,8 @@ module "eks" {
 
   cluster_name    = "test"
   cluster_version = "1.20"
-  vpc_id          = "vpc-5cac4721"
-  subnets         = ["subnet-bfb726b1", "subnet-6930ea48", "subnet-2f974849", "subnet-65a3cc28", "subnet-98e139c7"]
+  vpc_id          = aws_vpc.demo.id
+  subnets         = aws_subnet.demo[*].id
 
   worker_groups = [
     {
@@ -57,44 +95,7 @@ provider "kubernetes" {
   #   load_config_file       = false
 }
 
-# resource "kubernetes_service" "example" {
-#   metadata {
-#     name = "terraform-example"
-#   }
-#   spec {
-#     selector = {
-#       app = kubernetes_pod.example.metadata.0.labels.app
-#     }
-#     port {
-#       port        = 8080
-#       target_port = 80
-#     }
-
-#     type = "LoadBalancer"
-#   }
-# }
-
-# resource "kubernetes_pod" "example" {
-#   metadata {
-#     name = "terraform-example"
-#     labels = {
-#       app = "MyApp"
-#     }
-#   }
-
-#   spec {
-#     container {
-#       image = "nginx:1.7.9"
-#       name  = "example"
-#     }
-#   }
-# }
-
-# output "load_balancer_ip" {
-#   value = "${kubernetes_pod.example}"
-# }
-
-resource "kubernetes_deployment" "example" {
+resource "kubernetes_deployment" "kottam-cicd" {
   metadata {
     name = "kottam-cicd"
     labels = {
@@ -121,7 +122,7 @@ resource "kubernetes_deployment" "example" {
       spec {
         container {
           image = "ashishkr99/kottam-cicd:latest"
-          name  = "example"
+          name  = "kottam-cicd"
 
           resources {
             limits = {
@@ -139,7 +140,7 @@ resource "kubernetes_deployment" "example" {
   }
 }
 
-resource "kubernetes_service" "example" {
+resource "kubernetes_service" "kottam-cicd" {
   metadata {
     name = "kottam-cicd"
   }
@@ -154,6 +155,10 @@ resource "kubernetes_service" "example" {
 
     type = "LoadBalancer"
   }
+}
+
+output "kottam-cicd" {
+  value = "${kubernetes_service.kottam-cicd.status[0].load_balancer[0].ingress[0].hostname}:${kubernetes_service.kottam-cicd.spec[0].port[0].port}"
 }
 
 provider "helm" {
@@ -203,9 +208,24 @@ resource "helm_release" "kube-prometheus" {
   }
 }
 
-output "grafana" {
-  value = "${helm_release.grafana}"
-  sensitive = true
+data "kubernetes_service" "grafana" {
+  depends_on = [helm_release.grafana]
+  metadata {
+    name = "grafana"
+  }
 }
 
-#test
+output "grafana" {
+  value = "${data.kubernetes_service.grafana.status[0].load_balancer[0].ingress[0].hostname}:${data.kubernetes_service.grafana.spec[0].port[0].port}"
+}
+
+data "kubernetes_service" "kube-prometheus" {
+  depends_on = [helm_release.kube-prometheus]
+  metadata {
+    name = "kube-prometheus-prometheus"
+  }
+}
+
+output "kube-prometheus" {
+  value = "${data.kubernetes_service.kube-prometheus.status[0].load_balancer[0].ingress[0].hostname}:${data.kubernetes_service.kube-prometheus.spec[0].port[0].port}"
+}
